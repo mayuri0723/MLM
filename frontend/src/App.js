b0ebc36646bdb006524e9149fa2c6d09a91b42fa
mayuri0723 <mayurichavan66@gmail.com>
2023-03-01 10:43:15 +0530

    Registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState, useEffect } from 'react'
+import { Container } from 'react-bootstrap'
+// Routing
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Login from './components/Login';
+import Registration from './components/Registration';
 
 function App() {
   return (
-    <div className="App">
-     <Login/>
-    </div>
+    <Router history={history}>
+    {/* <Header /> */}
+    <Route path='/' component={MainPage} exact />
+        <Container>
+            <Route path='/' component={Login} exact  />
+            <Route path='/Registration' component={Registration} />
+           
+          
+        </Container>
+
+    <Footer />
+</Router>
   );
 }
 
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -76,10 +76,10 @@ const Login = () => {
               id="password"
               autoComplete="current-password"
             />
-            <FormControlLabel
+            {/* <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
-            />
+            /> */}
             <Button
               type="submit"
               fullWidth
@@ -89,13 +89,13 @@ const Login = () => {
               Sign In
             </Button>
             <Grid container>
-              <Grid item xs>
+              {/* <Grid item xs>
                 <Link href="#" variant="body2">
                   Forgot password?
                 </Link>
-              </Grid>
+              </Grid> */}
               <Grid item>
-                <Link href="#" variant="body2">
+                <Link href="/Registration" variant="body2">
                   {"Don't have an account? Sign Up"}
                 </Link>
               </Grid>
